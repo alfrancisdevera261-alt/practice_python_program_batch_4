bf682fefcd4785249508d6955937100b278d6918
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-18 19:24:06 +0800

    added a loop to get all numbers that have the duplicate number

--- displays_all_numbers_have_duplicate.py
+++ displays_all_numbers_have_duplicate.py
@@ -2,3 +2,9 @@ number_list = []
 for i in range(10):
     ask_number = int(input("Enter a number: "))
     number_list.append(ask_number)
+
+result = []
+for number_count in number_list:
+    if number_list.count(number_count) != 1:
+        result.append(number_count)
+print(result)
