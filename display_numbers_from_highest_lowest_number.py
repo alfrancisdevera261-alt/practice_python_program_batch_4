31e182abbe7dbb6b0cd051136e8d988e8dd0ca0c
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-18 20:36:41 +0800

    added the sort function and set it to reverse

--- display_numbers_from_highest_lowest_number.py
+++ display_numbers_from_highest_lowest_number.py
@@ -1,4 +1,6 @@
 number_list = []
 while True:
     ask_number = int(input("Enter a number: "))
-    number_list.append
+    number_list.append(ask_number)
+    number_list.sort(reverse=True)
+    print(number_list)
