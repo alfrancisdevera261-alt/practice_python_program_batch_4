e648ff508e66ad98747f8c3c1be0857bcd91695e
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-18 19:48:28 +0800

    make a function to count numbers duplcate in the list

--- displays_the_most_duplicate_number.py
+++ displays_the_most_duplicate_number.py
@@ -3,4 +3,6 @@ while True:
     ask_number = int(input("Enter number: "))
     number_list.append(ask_number)
     
+    for numbers in number_list:
+        number_count = number_list.count(numbers)
     
