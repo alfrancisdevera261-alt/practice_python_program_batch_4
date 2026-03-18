d8fca76be0257d9cea4eae442851e82ded0dd9b9
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-18 19:51:29 +0800

    added a function that check the nummber count and replace it with the most highest

--- displays_the_most_duplicate_number.py
+++ displays_the_most_duplicate_number.py
@@ -3,6 +3,12 @@ while True:
     ask_number = int(input("Enter number: "))
     number_list.append(ask_number)
     
+    most_duplicate = None
+    max_count = 0
     for numbers in number_list:
         number_count = number_list.count(numbers)
+        if number_count > max_count:
+            max_count = number_count
+            most_duplicate = numbers
+    print(f"Most duplicate so far: {most_duplicate} (appears {max_count} times)")
     
